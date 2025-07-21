59430d3b59fc14907331532dd357b702641c7112
Vladimir Ignatov <vlad.ignatov@gmail.com>
2025-07-21 10:18:38 -0400

    v2

--- data.js
+++ data.js
@@ -1,125 +1,149 @@
-// Chart format - array of 5 numbers:
-// [
-//     min,      // sample minimum
-//     Q1,       // lower quartile
-//     median,   // median
-//     Q3,       // upper quartile
-//     max       // sample maximum
-// ]
 const patients = [
     {
-        name: "Alex Johnson",
-        gender: "Male",
-        dob: "2010-01-05",
+        // Patient header and description
+        // ---------------------------------------------------------------------
+        name: "Teresa Anderson",
+        gender: "Female",
+        dob: "2020-03-15",
         mrn: "12345678",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque maximus euismod est, sit amet commodo sem commodo sed. Duis gravida sem ac nunc dignissim pretium. Etiam pretium purus quis tincidunt vulputate. Praesent hendrerit lacinia scelerisque. Nam metus mi, dapibus quis fringilla eget, finibus eget lorem. Suspendisse potenti. Aliquam porta porta massa. Etiam ac convallis enim. Nunc laoreet lacus justo, eget varius purus lobortis nec. Donec in urna diam. Nulla quis egestas neque.",
+        description: "Female; Age >= 4 years; White; UC (Ulcerative Colitis); Description TODO; Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque maximus euismod est, sit amet commodo sem commodo sed. Duis gravida sem ac nunc dignissim pretium. Etiam pretium purus quis tincidunt vulputate. Praesent hendrerit lacinia scelerisque. Nam metus mi, dapibus quis fringilla eget, finibus eget lorem. Suspendisse potenti. Aliquam porta porta massa. Etiam ac convallis enim. Nunc laoreet lacus justo, eget varius purus lobortis nec. Donec in urna diam. Nulla quis egestas neque.",
 
-        // Not sure what this will represent but it is used to render the red
-        // marker showing how the patient compares with the population
-        value: 14.7,
+        // Medications table and chart screenshot
+        // ---------------------------------------------------------------------
+        population: {
+            screenshot: {
+                src: "./uc_survival_per_drug_class.png",
+                href: "https://smart-cumulus-fabric-2bea3378447d.herokuapp.com/views/96"
+            },
+            tableRows: [
+                ["UC", "Aminosalicylate" , 202, { boxplot: { data: [0.18, 1.31, 2.91, 5.18, 9.61 ] }}],
+                ["UC", "Anti-integrin"   , 54 , { boxplot: { data: [0.06, 0.62, 1.34, 1.99, 5.11 ] }}],
+                ["UC", "Anti-interleukin", 49 , { boxplot: { data: [0.35, 0.9 , 1.47, 3.11, 7.24 ] }}],
+                ["UC", "Anti-TNF"        , 287, { boxplot: { data: [0.14, 1.03, 2.25, 4.46, 8.47 ] }}],
+                ["UC", "Corticosteroid"  , 320, { boxplot: { data: [0.11, 1.18, 2.93, 5.63, 11.11] }}],
+                ["UC", "Immunomodulator" , 226, { boxplot: { data: [0.23, 1.46, 3.16, 6.1 , 9.83 ] }}]
+            ]
+        },
+
+        // Main table
+        // ---------------------------------------------------------------------
+        value: 4, // The patient age in years we render over the boxplot charts
         populationData: [
             {
-                label: "IBD Subtype is <b>UC (Ulcerative Colitis)</b>",
-                responder: '<a href="#">30%</a>',
-                nonResponder: '<a href="#"><b>70%</b></a>'
+                label: 'IBD Subtype is <b class="text-success">UC (Ulcerative Colitis)</b>',
+                responder: '26%',
+                nonResponder: '<a href="https://smart-cumulus-fabric-2bea3378447d.herokuapp.com/views/70">74%</a>',
+                surgery: '<a href="https://smart-cumulus-fabric-2bea3378447d.herokuapp.com/views/84">41%</a>'
             },
             {
-                label: "UC Location is <b>E4: pancolitis (proximal to hepatic flexure)</b>",
-                responder: "40%",
-                nonResponder: "<b>60%</b>"
+                label: "UC Location is <b>E4: pancolitis</b> (proximal to hepatic flexure)",
+                responder: "28%",
+                nonResponder: '<a href="https://smart-cumulus-fabric-2bea3378447d.herokuapp.com/views/63">72%</a>',
+                surgery: '<a href="https://smart-cumulus-fabric-2bea3378447d.herokuapp.com/views/90">56%</a>'
             },
             {
                 label: "UC Severity: <b>S1: ever severe</b> (PUCAI >/= 65)",
-                responder: "<b>75%</b>",
-                nonResponder: "25%"
+                responder: "21%",
+                nonResponder: '<a href="https://smart-cumulus-fabric-2bea3378447d.herokuapp.com/views/42">79%</a>',
+                surgery: '<a href="https://smart-cumulus-fabric-2bea3378447d.herokuapp.com/views/91">69%</a>'
             },
             {
-                label: "Age at Dx of <b>12 months</b>",
-                responder   : [5, 10, 12, 16, 28], // [low, q1, median, q3, high]
-                nonResponder: [8,  9, 11, 18, 20], // [low, q1, median, q3, high]
+                label: "Age at Dx of <b>52 months</b> (4 years)",
+                responder   : { boxplot: { data: [1, 3, 8   , 11, 16], link: "https://smart-cumulus-fabric-2bea3378447d.herokuapp.com/views/93" }}, // 1,2,4,6,8,3,2,2,7,9,10,13,9,8,8,12,11,16,13       
+                nonResponder: { boxplot: { data: [1, 5, 12  , 26, 34], link: "https://smart-cumulus-fabric-2bea3378447d.herokuapp.com/views/93" }}, // 1,5,4,1,5,3,8,6,12,7,16,16,26,27,29,26,34,29,14   
+                surgery     : { boxplot: { data: [1, 2, 10.5, 20, 32], link: "https://smart-cumulus-fabric-2bea3378447d.herokuapp.com/views/92" }}, // 2,2,1,2,1,3,4,8,4,15,19,20,13,29,25,25,32,17      
             },
             {
-                label: "Sex of <b>Male</b>",
-                responder: "30%",
-                nonResponder: "<b>70%</b>"
+                label: "Sex of <b>Female</b>",
+                responder: "24%",
+                nonResponder: '<a href="https://smart-cumulus-fabric-2bea3378447d.herokuapp.com/views/78">76%</a>',
+                surgery: '<a href="https://smart-cumulus-fabric-2bea3378447d.herokuapp.com/views/87">39%</a>'
             },
             {
-                label: "Race of <b>Black or African American</b>",
-                responder: "28%",
-                nonResponder: "<b>72%</b>"
+                label: "Race of <b>White</b>",
+                responder: "21%",
+                nonResponder: '<a href="https://smart-cumulus-fabric-2bea3378447d.herokuapp.com/views/79">79%</a>',
+                surgery: '<a href="https://smart-cumulus-fabric-2bea3378447d.herokuapp.com/views/88">42%</a>'
             },
             {
-                label: "Dx to Anti-TNF initiation of <b>1 months</b>",
-                responder   : [5, 10, 19, 35, 38], // [low, q1, median, q3, high]
-                nonResponder: [8,  9, 11, 14, 20], // [low, q1, median, q3, high]
-            },
-            {
-                label: "Rare genetic variant with predicted effect in <b>IL10RA gene</b>",
-                responder: "<b>3%</b>",
-                nonResponder: "<b>93%</b>"
-            },
-            {
-                label: "Rare genetic variant with predicted effect in <b>LRBA gene</b>",
-                responder: "<b>5%</b>",
-                nonResponder: "<b>95%</b>"
+                label: "Rare genetic predicted effect in <b>PLCG2</b>",
+                responder: "29%",
+                nonResponder: '<a href="https://smart-cumulus-fabric-2bea3378447d.herokuapp.com/views/58">71%</a>',
+                surgery: '<a href="https://smart-cumulus-fabric-2bea3378447d.herokuapp.com/views/89">7%</a>'
             }
         ]
     },
     {
-        name: "Teresa Anderson",
-        gender: "Female",
-        dob: "2010-01-05",
+        // Patient header and description
+        // ---------------------------------------------------------------------
+        name: "Alex Johnson",
+        gender: "Male",
+        dob: "2020-01-05",
         mrn: "23456789",
-        description: "Cras tincidunt orci lectus, vitae euismod urna fermentum ut. Donec auctor condimentum urna, sed pharetra leo. Vestibulum blandit risus ut pellentesque ullamcorper. Morbi sit amet eros interdum, congue nibh in, semper velit. Donec vitae arcu id nisl pulvinar porta nec eget libero. Sed in nibh ut mauris interdum bibendum. In bibendum ultricies lorem. Integer viverra turpis ut libero egestas, non posuere augue tristique. In nec justo sapien. Vestibulum dignissim, felis vel eleifend ornare, ante lacus rhoncus velit, malesuada scelerisque mauris neque congue dolor. Aenean ultricies eu massa ut aliquam. Sed suscipit nunc sem. Vivamus nec blandit mauris, at facilisis justo. Maecenas faucibus ac velit id vestibulum. Proin venenatis blandit aliquet.",
+        description: "Male; Age >= 1 years; Black or African American; Crohn's Disease (CD); Description TODO; Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque maximus euismod est, sit amet commodo sem commodo sed. Duis gravida sem ac nunc dignissim pretium. Etiam pretium purus quis tincidunt vulputate. Praesent hendrerit lacinia scelerisque. Nam metus mi, dapibus quis fringilla eget, finibus eget lorem. Suspendisse potenti. Aliquam porta porta massa. Etiam ac convallis enim. Nunc laoreet lacus justo, eget varius purus lobortis nec. Donec in urna diam. Nulla quis egestas neque.",
+        
+        // Medications table and chart screenshot
+        // ---------------------------------------------------------------------
+        population: {
+            screenshot: {
+                src: "./cd_survival_per_drug_class.png",
+                href: "https://smart-cumulus-fabric-2bea3378447d.herokuapp.com/views/95"
+            },
+            tableRows: [
+                ["CD", "Aminosalicylate" , 114, { boxplot: { data: [ 0.35, 1.75, 3.21, 5.43,  9.61 ] }}],
+                ["CD", "Anti-integrin"   , 26 , { boxplot: { data: [ 0.41, 0.71, 1.34, 1.85,  5.51 ] }}],
+                ["CD", "Anti-interleukin", 41 , { boxplot: { data: [ 0.35, 1.09, 1.47, 3   ,  7.24 ] }}],
+                ["CD", "Anti-TNF"        , 212, { boxplot: { data: [ 0.16, 1.25, 2.73, 4.76,  8.71 ] }}],
+                ["CD", "Corticosteroid"  , 219, { boxplot: { data: [ 0.18, 1.41, 3.11, 6.07, 10.48 ] }}],
+                ["CD", "Immunomodulator" , 164, { boxplot: { data: [ 0.33, 1.66, 3.44, 6.36,  9.61 ] }}],
+            ]
+        },
 
-        // Not sure what this will represent but it is used to render the red
-        // marker showing how the patient compares with the population
-        value: 14.3,
+        // Main table
+        // ---------------------------------------------------------------------
+        value: 1, // The patient age in years we render over the boxplot charts
         populationData: [
             {
-                label: '<a href="https://smart-cumulus-fabric-2bea3378447d.herokuapp.com/views/40">IBD Subtype is <b>Crohn\'s Disease (CD)</b></a>',
-                responder: '57.5%',
-                nonResponder: '<b>42.5%</b>'
+                label: 'IBD Subtype is <b class="text-success">Crohn\'s Disease (CD)</b>',
+                responder: '22%',
+                nonResponder: '<a href="https://smart-cumulus-fabric-2bea3378447d.herokuapp.com/views/70">78%</a>',
+                surgery: '<a href="https://smart-cumulus-fabric-2bea3378447d.herokuapp.com/views/84">35%</a>'
             },
             {
-                label: "CD Location is <b>L3 (Ileocolonic)</b> and <b>L4a (Upper GI)</b>",
-                responder: "40%",
-                nonResponder: "<b>60%</b>"
+                label: "CD Location is <b>L3 (Ileocolonic)</b>",
+                responder: "16%",
+                nonResponder: '<a href="https://smart-cumulus-fabric-2bea3378447d.herokuapp.com/views/62">84%</a>',
+                surgery: '<a href="https://smart-cumulus-fabric-2bea3378447d.herokuapp.com/views/85">37%</a>'
             },
             {
-                label: '<a href="https://smart-cumulus-fabric-2bea3378447d.herokuapp.com/views/45">CD Behavior: <b>B2: stricturing</b></a>',
-                responder: "<b>56.7%</b>",
-                nonResponder: "43.3%"
+                label: 'CD Behavior: <b>B2: stricturing</b>',
+                responder: "21%",
+                nonResponder: '<a href="https://smart-cumulus-fabric-2bea3378447d.herokuapp.com/views/74">79%</a>',
+                surgery: '<a href="https://smart-cumulus-fabric-2bea3378447d.herokuapp.com/views/86">58%</a>'
             },
             {
-                label: '<a href="https://smart-cumulus-fabric-2bea3378447d.herokuapp.com/views/37">Age at Dx of <b>52 months</b></a>',
-                responder   : [5, 10, 12, 16, 28], // [low, q1, median, q3, high]
-                nonResponder: [8,  9, 11, 18, 20], // [low, q1, median, q3, high]
+                label: 'Age at Dx of <b>12 months</b>',
+                responder   : { boxplot: { data: [1, 3, 8   , 11, 16], link: "https://smart-cumulus-fabric-2bea3378447d.herokuapp.com/views/93" }}, // 1,2,4,6,8,3,2,2,7,9,10,13,9,8,8,12,11,16,13    
+                nonResponder: { boxplot: { data: [1, 5, 12  , 26, 34], link: "https://smart-cumulus-fabric-2bea3378447d.herokuapp.com/views/93" }}, // 1,5,4,1,5,3,8,6,12,7,16,16,26,27,29,26,34,29,14
+                surgery     : { boxplot: { data: [1, 2, 10.5, 20, 32] }}, // 2,2,1,2,1,3,4,8,4,15,19,20,13,29,25,25,32,17        ?
             },
             {
-                label: '<a href="https://smart-cumulus-fabric-2bea3378447d.herokuapp.com/views/41">Sex of <b>Female</b></a>',
-                responder: "57.9%",
-                nonResponder: "<b>42.1%</b>"
+                label: 'Sex of <b>Male</b>',
+                responder: "24%",
+                nonResponder: '<a href="https://smart-cumulus-fabric-2bea3378447d.herokuapp.com/views/75">76%</a>',
+                surgery: '<a href="https://smart-cumulus-fabric-2bea3378447d.herokuapp.com/views/87">41%</a>'
             },
             {
-                label: '<a href="https://smart-cumulus-fabric-2bea3378447d.herokuapp.com/views/39">Race of <b>Asian</b></a>',
-                responder: "77.8%",
-                nonResponder: "<b>22.2%</b>"
+                label: 'Race of <b>Black or African American</b>',
+                responder: "14%",
+                nonResponder: '<a href="https://smart-cumulus-fabric-2bea3378447d.herokuapp.com/views/77">86%</a>',
+                surgery: '<a href="https://smart-cumulus-fabric-2bea3378447d.herokuapp.com/views/88">30%</a>'
             },
             {
-                label: "Dx to Anti-TNF initiation of <b>1 months</b>",
-                responder   : [5, 10, 19, 35, 38], // [low, q1, median, q3, high]
-                nonResponder: [8,  9, 11, 14, 20], // [low, q1, median, q3, high]
-            },
-            {
-                label: '<a href="https://smart-cumulus-fabric-2bea3378447d.herokuapp.com/views/38">Rare genetic variant with predicted effect in <b>IL10RA gene</b></a>',
-                responder: '<b class="text-muted">-</b>',
-                nonResponder: '<b class="text-muted">-</b>'
-            },
-            {
-                label: '<a href="https://smart-cumulus-fabric-2bea3378447d.herokuapp.com/views/38">Rare genetic variant with predicted effect in <b>LRBA gene</b></a>',
-                responder: "<b>100%</b>",
-                nonResponder: "<b>0%</b>"
+                label: 'Rare genetic predicted effect in <b>STIM1</b>',
+                responder: "25%",
+                nonResponder: '<a href="https://smart-cumulus-fabric-2bea3378447d.herokuapp.com/views/58">75%</a>',
+                surgery: '<a href="https://smart-cumulus-fabric-2bea3378447d.herokuapp.com/views/89">47%</a>'
             }
         ]
     }
--- lib.js
+++ lib.js
@@ -1,3 +1,150 @@
+// @ts-nocheck
+
+const plotChartBaseConfig = {
+    chart: {
+        type: 'boxplot',
+        inverted: true,
+        backgroundColor: 'transparent',
+        style: { overflow: 'visible' }
+    },
+    title: null,
+    xAxis: {
+        visible: false
+    },
+    yAxis: {
+        visible: false,
+        maxPadding: 0,
+        minPadding: 0
+    },
+    plotOptions: {
+        boxplot: {
+            // boxDashStyle: 'Dash',
+            fillColor: '#FFFFFF',
+            lineWidth: 1,
+            medianColor: '#888888',
+            // medianDashStyle: 'ShortDot',
+            medianWidth: 2,
+            stemColor: '#888888',
+            stemDashStyle: 'ShortDash',
+            // stemWidth: 1,
+            whiskerColor: '#888888',
+            whiskerLength: '60%',
+            whiskerWidth: 2,
+            color    : "#888888",
+            animation: false,
+        }
+    },
+    series: [],
+    credits: { enabled: false },
+    legend: { enabled: false },
+    tooltip: {
+        shared: true,
+        useHTML: true,
+        outside: true,
+        animation: false,
+        hideDelay: 0,
+        style: {
+            fontSize: '16px',
+            fontFamily: 'inherit',
+            color: 'inherit'
+        },
+        formatter () {
+            return `<b>Distribution</b><br/>
+            Min: ${this.low}<br/>
+            Q1: ${this.q1}<br/>
+            Median: <b>${this.median}</b><br/>
+            Q3: ${this.q3}<br/>
+            Max: ${this.high}`;
+        }
+    },
+};
+
+function populationPlotChart(container, options) {
+    const cfg = {
+        ...plotChartBaseConfig,
+        chart: {
+            ...plotChartBaseConfig.chart,
+            spacing: [2, 2, 2, 2],
+            height: options.height || 54,
+            width: options.width || 200
+        },
+        yAxis: {
+            ...plotChartBaseConfig.yAxis,
+            min: options.data[0],
+            max: options.data[4],
+        },
+        series: [
+            {
+                type: 'boxplot',
+                data: [options.data],
+            }
+        ]
+    };
+    Highcharts.chart(container, cfg);
+}
+
+function plotChart(container, options, value) {
+
+    const cfg = {
+        ...plotChartBaseConfig,
+        chart: {
+            ...plotChartBaseConfig.chart,
+            spacing: [0, 2, 4, 2],
+            height: options.height || 54,
+            // width: options.width || 200
+        },
+        yAxis: {
+            ...plotChartBaseConfig.yAxis,
+            min: options.data[0],
+            max: options.data[4],
+        },
+        series: [
+            {
+                type: 'boxplot',
+                data: [options.data]
+            }
+        ]
+    };
+
+    if (value || value === 0) {
+        cfg.series.push({
+            name: 'Patient',
+            type: 'scatter',
+            data: [[0, value]],
+            marker: {
+                enabled: false // hide the dot
+            },
+            animation: false,
+            dataLabels: {
+                enabled: true,
+                useHTML: true,
+                formatter: function () {
+                    return `<div style="margin-top:1px;line-height:20px">
+                    <span style="color:red;font-size: 18px;vertical-align: top">🔺</span>
+                    </div>`;
+                },
+                align: 'left',
+                verticalAlign: 'bottom',
+                y: 0
+            },
+            enableMouseTracking: false
+        });
+
+        cfg.tooltip.formatter = () => {
+            const bp = options.data;
+            return `<b>Distribution:</b><br/>
+            Min: ${bp[0]}<br/>
+            Q1: ${bp[1]}<br/>
+            Median: ${bp[2]}<br/>
+            Q3: ${bp[3]}<br/>
+            Max: ${bp[4]}<br/>
+            <hr style="margin:4px 0;">
+            <b>Patient value:</b> ${value}`;
+        };
+    }
+
+    Highcharts.chart(container, cfg);
+}
 
 function formatDOB(dateStr) {
     const date = new Date(dateStr);
@@ -22,4 +169,34 @@ function buildPatientHeader(patient) {
             <div class="text-muted">${buildPatientDemographics(patient)}</div>
         </div>
     `);
+}
+
+function renderPatientHeader(patient) {
+    $('#patient-header').html(`
+        <div class="text-success" style="font-size:60px;line-height:1.2">
+            <i class="bi bi-person-circle"></i>
+        </div>
+        <div>
+            <div class="fw-bold fs-4">${patient.name}</div>
+            <div class="text  -success">${buildPatientDemographics(patient)}</div>
+        </div>
+    `);
+    $('#patient-description').html(`<p class="small text-muted mt-2">${patient.description}</p>`);
+}
+
+function renderPopulationHeader(data) {
+    $(".chart-screenshot").attr("src", data.screenshot.src).parent().attr("href", data.screenshot.href)
+    const $tbody = $("#population-table-body").empty()
+
+    data.tableRows.forEach(row => {
+        $(
+            `<tr>
+                <td class="text-start text-muted">${row[0]}</td>
+                <td class="text-start text-success fw-semibold">${row[1]}</td>
+                <td class="text-center text-muted">${row[2]}</td>
+                <td class="text-start hc-container-small" data-boxplot="${row[3].boxplot.data.join(",")}"></td>
+                <td class="text-start text-muted" style="width:3em"><span class="badge bg-success d-block">${row[3].boxplot.data[2]}</span></td>
+            </tr>`
+        ).appendTo($tbody)
+    })
 }
